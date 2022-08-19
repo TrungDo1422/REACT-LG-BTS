c064df3b034630d89952d41671eefe0a6f85edbd
Thế Quý Nguyễn <quynguyena52000@gmail.com>
2022-08-19 16:33:38 +0700

    Create UI

--- src/components/admin/main/index.js
+++ src/components/admin/main/index.js
@@ -1,113 +1,227 @@
-import React from 'react';
-import './main.scss';
-import Banner from '~/components/Banner'
-import About from '~/components/About';
-import Categories from '~/components/Categories';
-import Popular from '~/components/Popular';
-import CTA from '~/components/CTA';
-import Concern from '~/components/Concern';
-import Testimonial from '~/components/Testimonial';
 import Image from '~/assets/image/devplus/logo_loading.png';
+import './main.scss';
 
 const Main = () => {
-    return (<div>
-        <div className="content-wrapper">
-            <div className='modal_container' id='exit'>
-                <Banner id='banner'/>
-                <About id='about'/>
-                <Categories id='category' />
-                <Popular />
-                <CTA />
-                <Concern />
-                <Testimonial />
-                {/* ---------------banner---------------- */}
-                <div className='banner_modal' id='banner-modal'>
-                    <div className='modal_body'>
-                        <a className='close-btn' href='#banner'>&times;</a>
-                        <div className='table_card'>
-                            <table class="table">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Title</th>
-                                        <th scope="col">Content</th>
-                                        <th scope="col">Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>Otto</td>
-                                        <td>Otto</td>
-                                        <td>
-                                            <a className='btn btn-primary'>Update</a>|
-                                            <a className='btn btn-danger'>Delete</a>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
+    return (
+        <div>
+            <div className="content-wrapper">
+                <div className='modal_container' id='exit'>
+                    <div className='m-5'></div>
+
+                    {/* ---------------banner---------------- */}
+                    <div className='banner_modal' id='banner-modal'>
+                        <div className='modal_body'>
+                            <a className='close-btn' href='#exit'>&times;</a>
+                            <div className='table_card'>
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Title</th>
+                                            <th scope="col">Content</th>
+                                            <th scope="col">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Otto</td>
+                                            <td>Otto</td>
+                                            <td>
+                                                <a className='btn btn-primary'>Update</a>|
+                                                <a className='btn btn-danger'>Delete</a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
-                </div>
-                {/* ---------------------------about-------------- */}
-                <div className='about_modal' id='about-modal'>
-                    <div className='modal_body'>
-                        <a className='close-btn' href='#about'>&times;</a>
-                        <div className='table_card'>
-                            <table class="table">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Title</th>
-                                        <th scope="col">Content</th>
-                                        <th scope="col">Description</th>
-                                        <th scope="col">Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>Otto</td>
-                                        <td>hihi</td>
-                                        <td>Otto</td>
-                                        <td>
-                                            <a className='btn btn-primary'>Update</a>|
-                                            <a className='btn btn-danger'>Delete</a>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                    {/* ---------------------------about-------------- */}
+                    <div className='about_modal' id='about-modal'>
+                        <div className='modal_body'>
+                            <a className='close-btn' href='#exit'>&times;</a>
+                            <div className='table_card'>
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Title</th>
+                                            <th scope="col">Content</th>
+                                            <th scope="col">Description</th>
+                                            <th scope="col">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Otto</td>
+                                            <td>hihi</td>
+                                            <td>Otto</td>
+                                            <td>
+                                                <a className='btn btn-primary'>Update</a>|
+                                                <a className='btn btn-danger'>Delete</a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
-                </div>
-                {/* ----------------------categories------------------- */}
-                <div className='category_modal' id='category-modal'>
-                    <div className='modal_body'>
-                        <a className='close-btn' href='#about'>&times;</a>
-                        <div className='table_card'>
-                            <table class="table">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Title</th>
-                                        <th scope="col">Content</th>
-                                        <th scope="col">Icons</th>
-                                        <th scope="col">Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>Otto</td>
-                                        <td>hihi</td>
-                                        <td><img src={Image} alt='# '/></td>
-                                        <td>
-                                            <a className='btn btn-primary'>Update</a>|
-                                            <a className='btn btn-danger'>Delete</a>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                    {/* ----------------------categories------------------- */}
+                    <div className='category_modal' id='category-modal'>
+                        <div className='modal_body'>
+                            <a className='close-btn' href='#exit'>&times;</a>
+                            <div className='table_card'>
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Icons</th>
+                                            <th scope="col">Title</th>
+                                            <th scope="col">Content</th>
+                                            <th scope="col text-center">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr> <td><img src={Image} alt='# ' /></td>
+                                            <td>Otto</td>
+                                            <td>hihi</td>
+
+                                            <td>
+                                                <a className='btn btn-primary'>Create</a>|
+                                                <a className='btn btn-secondary'>Update</a>|
+                                                <a className='btn btn-danger'>Delete</a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    {/* ----------------------Popular------------------- */}
+                    <div className='popular_modal' id='popular-modal'>
+                        <div className='modal_body'>
+                            <a className='close-btn' href='#exit'>&times;</a>
+                            <div className='table_card'>
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Images</th>
+                                            <th scope="col">Content</th>
+                                            <th scope="col text-center">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr> <td><img src={Image} alt='# ' /></td>
+                                            <td>hihi</td>
+                                            <td>
+                                                <a className='btn btn-primary'>Create</a>|
+                                                <a className='btn btn-secondary'>Update</a>|
+                                                <a className='btn btn-danger'>Delete</a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    {/* ----------------------CTA------------------- */}
+                    <div className='cta_modal' id='cta-modal'>
+                        <div className='modal_body'>
+                            <a className='close-btn' href='#exit'>&times;</a>
+                            <div className='table_card'>
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Images</th>
+                                            <th scope="col">Title</th>
+                                            <th scope="col">Content</th>
+                                            <th scope="col text-center">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <img src={Image} alt='# ' />
+                                            </td>
+                                            <td>helo man</td>
+                                            <td>hihi</td>
+                                            <td>
+                                                <a className='btn btn-primary'>Create</a>|
+                                                <a className='btn btn-secondary'>Update</a>|
+                                                <a className='btn btn-danger'>Delete</a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    {/* ----------------------Concerns------------------- */}
+                    <div className='concern_modal' id='concern-modal'>
+                        <div className='modal_body'>
+                            <a className='close-btn' href='#exit'>&times;</a>
+                            <div className='table_card'>
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Title</th>
+                                            <th scope="col">Content</th>
+                                            <th scope="col">Description</th>
+                                            <th scope="col">Images</th>
+                                            <th scope="col text-center">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>hihi</td>
+                                            <td>hihi</td>
+                                            <td>hihi</td>
+                                            <td>
+                                                <img src={Image} alt='# ' />
+                                            </td>
+                                            <td>
+                                                <a className='btn btn-primary'>Create</a>|
+                                                <a className='btn btn-secondary'>Update</a>|
+                                                <a className='btn btn-danger'>Delete</a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    {/* ----------------------Alumni------------------- */}
+                    <div className='alumni_modal' id='alumni-modal'>
+                        <div className='modal_body'>
+                            <a className='close-btn' href='#exit'>&times;</a>
+                            <div className='table_card'>
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Content</th>
+                                            <th scope="col">Images</th>
+                                            <th scope="col">Name</th>
+                                            <th scope="col">Level</th>
+                                            <th scope="col text-center">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>hello number 1</td>
+                                            <td><img src={Image} alt='# ' /></td>
+                                            <td>Tien Tinh</td>
+                                            <td>member devplus ++</td>
+                                            <td>
+                                                <a className='btn btn-primary'>Create</a>|
+                                                <a className='btn btn-secondary'>Update</a>|
+                                                <a className='btn btn-danger'>Delete</a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
     )
 }
 
