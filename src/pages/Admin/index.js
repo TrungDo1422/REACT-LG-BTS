b398b51948bc1240244a364f5e50035a3907cfa1
quy nguyen <quy.nguyen@devplus.edu.vn>
2022-08-23 09:49:54 +0700

    fix warm and layout

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -4,9 +4,11 @@ import Main from '~/components/admin/main';
 const Admin = () => {
     return (
         <div className="wrapper">
-            <div className="admin">{/* <Main /> */}</div>
-        </div>
-    );
-};
+            <div className='admin'>
+                <Main />
+            </div>
 
+        </div>
+    )
+}
 export default Admin;
